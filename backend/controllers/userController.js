cec3851f29e13939241362a5888a863a77332fc8
Ketan Dabre <ketandabre99per@gmail.com>
2023-08-26 18:34:30 +0530

    added register user endpoint, genetated JWT and save cookie and Auth user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,8 @@
 import asyncHandler from "express-async-handler";
-import User from '../models/userModel.js';
-
+import User from "../models/userModel.js";
+import generateToken from "../utils/genetateToken.js";
 
 const authUser = asyncHandler(async (req, res) => {
-  //   res.status(401);
-  //   throw new Error("Something went wrong");
-
   res.status(200).json({ message: "Auth User" });
 });
 
@@ -17,7 +14,25 @@ const registerUser = asyncHandler(async (req, res) => {
 
   if (userExists) {
     res.status(400);
-    throw new Error('User already exists')
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data");
   }
   res.status(200).json({ message: "Register User" });
 });
@@ -29,6 +44,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 const getUserProfile = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "User Profile" });
 });
+
 const updateUserProfile = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Update User Profile" });
 });
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,24 +1,35 @@
 import mongoose from "mongoose";
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+import bcrypt from "bcryptjs";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-        required: true,
-    
-  },
-}, {
-      timestamps :true  // to add createdAt and updatedAt field in the schema
+  {
+    timestamps: true, // to add createdAt and updatedAt field in the schema
+  }
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password'))
+    return next();
+
+  const salt = await bcrypt.genSalt(15);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
-
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
